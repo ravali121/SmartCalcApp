1a65a68e5b40340f18f7468e9f3e3f8888b4666a
bhavadeepk <bhavadeep.k93@hotmail.com>
2017-07-26 22:52:05 -0700

    Login & NavDraw

--- Android/app/src/main/java/com/student/smartcalcapp/LoginActivity.java
+++ Android/app/src/main/java/com/student/smartcalcapp/LoginActivity.java
@@ -2,12 +2,26 @@ package com.student.smartcalcapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private Button b_teacher ;
+    private Button b_student ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.login_trail);
+        b_teacher = (Button)findViewById(R.id.b_teacher);
+        b_student = (Button)findViewById(R.id.b_student);
+        b_student.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                b_teacher.setEnabled(true);
+                b_student.setEnabled(false);
+            }
+        });
     }
 }
